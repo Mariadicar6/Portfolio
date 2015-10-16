ecf601f2d3d780fbf40c543e48d69f982f501ee8
Mariadicar <mariadicar.28@gmail.com>
2015-10-16 16:28:21 -0500

    Imagen habilidades

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(window).scroll(function () {
      var sc = $(window).scrollTop()
-    if (sc > 640) {
+    if (sc > 480) {
         $("#header-sroll").addClass("small")
     } else {
         $("#header-sroll").removeClass("small")
