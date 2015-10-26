496d70b3d3d972bef51274e8bf1d3ec9f8a625ba
Maria <mariadicar.28@gmail.com>
2015-10-26 14:49:02 -0500

    Efecto hover redes sociales

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,15 @@ jQuery(function () {
 	});
 	
 }());
+
+
+$(".mv").hover(
+  function () {
+    $(this).addClass('animated swing');
+  }, 
+  function () {
+    $(this).removeClass('animated swing');
+  }
+);
+
+
