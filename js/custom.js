085ee2426d64b5ba8ff3a061724f41d4e013744e
Maria <mariadicar.28@gmail.com>
2015-10-27 13:31:49 -0500

    Ocultar menu en responsive

--- js/custom.js
+++ js/custom.js
@@ -25,4 +25,10 @@ $(".mv").hover(
   }
 );
 
-
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
